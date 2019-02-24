2e8c5805a6ae44efa33eb8a1b38de3177b6089c3
Stedman Halliday <stedman.rh@gmail.com>
2019-02-24 12:30:12 -0800

    needs grid

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
 
     $('#menu-toggle').click(function(){
-        $('#menu-list').toggleClass('open', function(){
-            $(this).toggle();
-        });
+        $('#menu-list').toggleClass('open');
     })
 
 })
